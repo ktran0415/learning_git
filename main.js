083d1c8e7966833722386aa02a0d95463920ca65
ktran0415 <ktran0415@gmail.com>
2018-08-22 19:49:11 -0600

    committing for safety

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(){
 //the entire game is inside here
 //variables
+var player
 var name = document.getElementById('name')
 var startButton = document.getElementById('start-button')
-name.addEventListener('keyup', function(){
+var top = document.getElementById('top')
+name.addEventListener('keyup', function(event){
   startButton.className=""
+} else {
+  startButton.className="hide"
+
 }
+)
 
 //functions
+function startGame() {
+  player = name.value
+  startButton.className = "hide"
+  var label = document.createElement('p')
+  label.innerHTML = 'Welcome ' + player 
+  label. className = 'center'
+  top
+}
+startButton.addEventListener('click', startGame)
 })
