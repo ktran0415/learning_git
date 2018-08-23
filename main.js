9bcd78e82cc59fcd1c6a4b2349dd80a8fe95c694
ktran0415 <ktran0415@gmail.com>
2018-08-22 19:28:43 -0600

    here you go jimmy

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+//the entire game is inside here
+//variables
+var name = document.getElementById('name')
+var startButton = document.getElementById('start-button')
+name.addEventListener('keyup', function(){
+  startButton.className=""
+}
+
+//functions
+})
